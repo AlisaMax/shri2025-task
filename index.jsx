6ec0c0fd8fa38a3e583707a2f0ffb3b1fdf04075
Maxim <alisamaksmax@gmail.com>
2025-07-02 14:18:43 +0300

    ifx

--- index.jsx
+++ index.jsx
@@ -58,177 +58,169 @@ function Event(props) {
     </li>;
 }
 
-
-//     all: {
-//         title: 'Все',
-//         items: [{
-//             icon: 'light2',
-//             iconLabel: 'Освещение',
-//             title: 'Xiaomi Yeelight LED Smart Bulb',
-//             subtitle: 'Включено'
-//         }, {
-//             icon: 'light',
-//             iconLabel: 'Освещение',
-//             title: 'D-Link Omna 180 Cam',
-//             subtitle: 'Включится в 17:00'
-//         }, {
-//             icon: 'temp',
-//             iconLabel: 'Температура',
-//             title: 'Elgato Eve Degree Connected',
-//             subtitle: 'Выключено до 17:00'
-//         }, {
-//             icon: 'light',
-//             iconLabel: 'Освещение',
-//             title: 'LIFX Mini Day & Dusk A60 E27',
-//             subtitle: 'Включится в 17:00'
-//         }, {
-//             icon: 'light2',
-//             iconLabel: 'Освещение',
-//             title: 'Xiaomi Mi Air Purifier 2S',
-//             subtitle: 'Включено'
-//         }, {
-//             icon: 'light',
-//             iconLabel: 'Освещение',
-//             title: 'Philips Zhirui',
-//             subtitle: 'Включено'
-//         }, {
-//             icon: 'light',
-//             iconLabel: 'Освещение',
-//             title: 'Philips Zhirui',
-//             subtitle: 'Включено'
-//         }, {
-//             icon: 'light2',
-//             iconLabel: 'Освещение',
-//             title: 'Xiaomi Mi Air Purifier 2S',
-//             subtitle: 'Включено'
-//         }]
-//     },
-//     kitchen: {
-//         title: 'Кухня',
-//         items: [{
-//             icon: 'light2',
-//             iconLabel: 'Освещение',
-//             title: 'Xiaomi Yeelight LED Smart Bulb',
-//             subtitle: 'Включено'
-//         }, {
-//             icon: 'temp',
-//             iconLabel: 'Температура',
-//             title: 'Elgato Eve Degree Connected',
-//             subtitle: 'Выключено до 17:00'
-//         }]
-//     },
-//     hall: {
-//         title: 'Зал',
-//         items: [{
-//             icon: 'light',
-//             iconLabel: 'Освещение',
-//             title: 'Philips Zhirui',
-//             subtitle: 'Выключено'
-//         }, {
-//             icon: 'light2',
-//             iconLabel: 'Освещение',
-//             title: 'Xiaomi Mi Air Purifier 2S',
-//             subtitle: 'Выключено'
-//         }]
-//     },
-//     lights: {
-//         title: 'Лампочки',
-//         items: [{
-//             icon: 'light',
-//             iconLabel: 'Освещение',
-//             title: 'D-Link Omna 180 Cam',
-//             subtitle: 'Включится в 17:00'
-//         }, {
-//             icon: 'light',
-//             iconLabel: 'Освещение',
-//             title: 'LIFX Mini Day & Dusk A60 E27',
-//             subtitle: 'Включится в 17:00'
-//         }, {
-//             icon: 'light2',
-//             iconLabel: 'Освещение',
-//             title: 'Xiaomi Mi Air Purifier 2S',
-//             subtitle: 'Включено'
-//         }, {
-//             icon: 'light',
-//             iconLabel: 'Освещение',
-//             title: 'Philips Zhirui',
-//             subtitle: 'Включено'
-//         }]
-//     },
-//     cameras: {
-//         title: 'Камеры',
-//         items: [{
-//             icon: 'light2',
-//             iconLabel: 'Освещение',
-//             title: 'Xiaomi Mi Air Purifier 2S',
-//             subtitle: 'Включено'
-//         }]
-//     }
-// };
-const TABS = [
-    { key: 'all', title: 'Все' },
-    { key: 'kitchen', title: 'Кухня' },
-    { key: 'hall', title: 'Зал' },
-    { key: 'lights', title: 'Лампочки' },
-    { key: 'cameras', title: 'Камеры' }
-];
-
-const DEVICES = [
-    {
-        icon: 'light2',
-        iconLabel: 'Освещение',
-        title: 'Xiaomi Yeelight LED Smart Bulb',
-        subtitle: 'Включено',
-        type: 'kitchen',
+const TABS = {
+    all: {
+        title: 'Все',
+        items: [{
+            icon: 'light2',
+            iconLabel: 'Освещение',
+            title: 'Xiaomi Yeelight LED Smart Bulb',
+            subtitle: 'Включено'
+        }, {
+            icon: 'light',
+            iconLabel: 'Освещение',
+            title: 'D-Link Omna 180 Cam',
+            subtitle: 'Включится в 17:00'
+        }, {
+            icon: 'temp',
+            iconLabel: 'Температура',
+            title: 'Elgato Eve Degree Connected',
+            subtitle: 'Выключено до 17:00'
+        }, {
+            icon: 'light',
+            iconLabel: 'Освещение',
+            title: 'LIFX Mini Day & Dusk A60 E27',
+            subtitle: 'Включится в 17:00'
+        }, {
+            icon: 'light2',
+            iconLabel: 'Освещение',
+            title: 'Xiaomi Mi Air Purifier 2S',
+            subtitle: 'Включено'
+        }, {
+            icon: 'light',
+            iconLabel: 'Освещение',
+            title: 'Philips Zhirui',
+            subtitle: 'Включено'
+        }, {
+            icon: 'light',
+            iconLabel: 'Освещение',
+            title: 'Philips Zhirui',
+            subtitle: 'Включено'
+        }, {
+            icon: 'light2',
+            iconLabel: 'Освещение',
+            title: 'Xiaomi Mi Air Purifier 2S',
+            subtitle: 'Включено'
+        }]
     },
-    {
-        icon: 'temp',
-        iconLabel: 'Температура',
-        title: 'Elgato Eve Degree Connected',
-        subtitle: 'Выключено до 17:00',
-        type: 'kitchen',
+    kitchen: {
+        title: 'Кухня',
+        items: [{
+            icon: 'light2',
+            iconLabel: 'Освещение',
+            title: 'Xiaomi Yeelight LED Smart Bulb',
+            subtitle: 'Включено'
+        }, {
+            icon: 'temp',
+            iconLabel: 'Температура',
+            title: 'Elgato Eve Degree Connected',
+            subtitle: 'Выключено до 17:00'
+        }]
     },
-    {
-        icon: 'light',
-        iconLabel: 'Освещение',
-        title: 'Philips Zhirui',
-        subtitle: 'Выключено',
-        type: 'hall',
+    hall: {
+        title: 'Зал',
+        items: [{
+            icon: 'light',
+            iconLabel: 'Освещение',
+            title: 'Philips Zhirui',
+            subtitle: 'Выключено'
+        }, {
+            icon: 'light2',
+            iconLabel: 'Освещение',
+            title: 'Xiaomi Mi Air Purifier 2S',
+            subtitle: 'Выключено'
+        }]
     },
-    {
-        icon: 'light2',
-        iconLabel: 'Освещение',
-        title: 'Xiaomi Mi Air Purifier 2S',
-        subtitle: 'Выключено',
-        type: 'hall',
+    lights: {
+        title: 'Лампочки',
+        items: [{
+            icon: 'light',
+            iconLabel: 'Освещение',
+            title: 'D-Link Omna 180 Cam',
+            subtitle: 'Включится в 17:00'
+        }, {
+            icon: 'light',
+            iconLabel: 'Освещение',
+            title: 'LIFX Mini Day & Dusk A60 E27',
+            subtitle: 'Включится в 17:00'
+        }, {
+            icon: 'light2',
+            iconLabel: 'Освещение',
+            title: 'Xiaomi Mi Air Purifier 2S',
+            subtitle: 'Включено'
+        }, {
+            icon: 'light',
+            iconLabel: 'Освещение',
+            title: 'Philips Zhirui',
+            subtitle: 'Включено'
+        }]
     },
-    {
-        icon: 'light',
-        iconLabel: 'Освещение',
-        title: 'D-Link Omna 180 Cam',
-        subtitle: 'Включится в 17:00',
-        type: 'lights',
-    },
-    {
-        icon: 'light',
-        iconLabel: 'Освещение',
-        title: 'LIFX Mini Day & Dusk A60 E27',
-        subtitle: 'Включится в 17:00',
-        type: 'lights',
-    },
-    {
-        icon: 'light2',
-        iconLabel: 'Освещение',
-        title: 'Xiaomi Mi Air Purifier 2S',
-        subtitle: 'Включено',
-        type: 'cameras',
+    cameras: {
+        title: 'Камеры',
+        items: [{
+            icon: 'light2',
+            iconLabel: 'Освещение',
+            title: 'Xiaomi Mi Air Purifier 2S',
+            subtitle: 'Включено'
+        }]
     }
-];
+};
+// const DEVICES = [
+//     {
+//         icon: 'light2',
+//         iconLabel: 'Освещение',
+//         title: 'Xiaomi Yeelight LED Smart Bulb',
+//         subtitle: 'Включено',
+//         type: 'kitchen',
+//     },
+//     {
+//         icon: 'temp',
+//         iconLabel: 'Температура',
+//         title: 'Elgato Eve Degree Connected',
+//         subtitle: 'Выключено до 17:00',
+//         roo m: 'kitchen',
+//     },
+//     {
+//         icon: 'light',
+//         iconLabel: 'Освещение',
+//         title: 'Philips Zhirui',
+//         subtitle: 'Выключено',
+//         type: 'hall',
+//     },
+//     {
+//         icon: 'light2',
+//         iconLabel: 'Освещение',
+//         title: 'Xiaomi Mi Air Purifier 2S',
+//         subtitle: 'Выключено',
+//         type: 'hall',
+//     },
+//     {
+//         icon: 'light',
+//         iconLabel: 'Освещение',
+//         title: 'D-Link Omna 180 Cam',
+//         subtitle: 'Включится в 17:00',
+//         type: 'lights',
+//     },
+//     {
+//         icon: 'light',
+//         iconLabel: 'Освещение',
+//         title: 'LIFX Mini Day & Dusk A60 E27',
+//         subtitle: 'Включится в 17:00',
+//         type: 'lights',
+//     },
+//     {
+//         icon: 'light2',
+//         iconLabel: 'Освещение',
+//         title: 'Xiaomi Mi Air Purifier 2S',
+//         subtitle: 'Включено',
+//         type: 'cameras',
+//     }
+// ];
 
 // for (let i = 0; i < 6; ++i) {
 //     TABS.all.items = [...TABS.all.items, ...TABS.all.items];
 // }
-// const TABS_KEYS = Object.keys(TABS);
+const TABS_KEYS = Object.keys(TABS);
 
 function Main() {
     const ref = React.useRef();
@@ -363,63 +355,57 @@ function Main() {
             </ul>
         </section>
 
-
         <section className="section main__devices">
             <div className="section__title">
-                <h2 className="section__title-header">Избранные устройства</h2>
+                <h2 className="section__title-header">
+                    Избранные устройства
+                </h2>
 
-                <select
-                    className="section__select"
-                    defaultValue="all"
-                    onInput={onSelectInput}
-                >
-                    {TABS.map(tab => (
-                        <option key={tab.key} value={tab.key}>
-                            {tab.title}
+                <select className="section__select" defaultValue="all" onInput={onSelectInput}>
+                    {TABS_KEYS.map(key =>
+                        <option key={key} value={key}>
+                            {TABS[key].title}
                         </option>
-                    ))}
+                    )}
                 </select>
 
                 <ul role="tablist" className="section__tabs">
-                    {TABS.map(tab => (
+                    {TABS_KEYS.map(key =>
                         <li
-                            key={tab.key}
+                            key={key}
                             role="tab"
-                            aria-selected={tab.key === activeTab ? 'true' : 'false'}
-                            tabIndex={tab.key === activeTab ? '0' : undefined}
-                            className={'section__tab' + (tab.key === activeTab ? ' section__tab_active' : '')}
-                            id={`tab_${tab.key}`}
-                            aria-controls={`panel_${tab.key}`}
-                            onClick={() => setActiveTab(tab.key)}
+                            aria-selected={key === activeTab ? 'true' : 'false'}
+                            tabIndex={key === activeTab ? '0' : undefined}
+                            className={'section__tab' + (key === activeTab ? ' section__tab_active' : '')}
+                            id={`tab_${key}`}
+                            aria-controls={`panel_${key}`}
+                            onClick={() => setActiveTab(key)}
                         >
-                            {tab.title}
+                            {TABS[key].title}
                         </li>
-                    ))}
+                    )}
                 </ul>
             </div>
 
             <div className="section__panel-wrapper" ref={ref}>
-                <div
-                    role="tabpanel"
-                    className="section__panel"
-                    id={`panel_${activeTab}`}
-                    aria-labelledby={`tab_${activeTab}`}
-                >
-                    <ul className="section__panel-list">
-                        {DEVICES
-                            .filter(device => activeTab === 'all' || device.type === activeTab)
-                            .map((item, index) => (
-                                <Event key={index} {...item} onSize={onSize} />
-                            ))}
-                    </ul>
-                </div>
-
-                {hasRightScroll && (
-                    <div className="section__arrow" onClick={onArrowCLick}></div>
+                {TABS_KEYS.map(key =>
+                    <div key={key} role="tabpanel" className={'section__panel' + (key === activeTab ? '' : ' section__panel_hidden')} aria-hidden={key === activeTab ? 'false' : 'true'} id={`panel_${key}`} aria-labelledby={`tab_${key}`}>
+                        <ul className="section__panel-list">
+                            {TABS[key].items.map((item, index) =>
+                                <Event
+                                    key={index}
+                                    {...item}
+                                    onSize={onSize}
+                                />
+                            )}
+                        </ul>
+                    </div>
                 )}
+                {hasRightScroll &&
+                    <div className="section__arrow" onClick={onArrowCLick}></div>
+                }
             </div>
         </section>
-
     </main>;
 }
 
@@ -433,3 +419,4 @@ setTimeout(() => {
     );
 }, 100);
 
+// export default Main();
